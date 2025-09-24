837d0bfef7fdb3c4d923c1a2687c1f47f01db205
Kevin Rosengren <rosenkev4@gmail.com>
2025-09-23 22:58:24 -0500

    Added struct and user interface function to ask user about the task that is due

--- user_interface/ui.go
+++ user_interface/ui.go
@@ -3,6 +3,7 @@ package userinterface
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
 func DisplayMain() int {
@@ -15,4 +16,39 @@ func DisplayMain() int {
 	fmt.Scanln(&userChoice)
 
 	return userChoice
+}
+
+type Todo struct {
+	Description string
+	Completed   bool
+	DueDate     time.Time
+}
+
+func DisplayAddTask() *Todo {
+	fmt.Println("\t---Add A Task---")
+	fmt.Println(strings.Repeat("_", 20))
+
+	fmt.Println("Enter a description for your task:")
+	var description string
+	fmt.Scanln(&description)
+
+	fmt.Println("Enter the day of the month it is due:")
+	var dayDue int
+	fmt.Scanln(&dayDue)
+
+	fmt.Println("Enter the month (numerically) it is due:")
+	var monthDue time.Month
+	fmt.Scanln(&monthDue)
+
+	fmt.Println("Enter the year that it is due:")
+	var yearDue int
+	fmt.Scanln(&yearDue)
+
+	dueDate := time.Date(yearDue, monthDue, dayDue, 0, 0, 0, 0, time.UTC)
+
+	return &Todo{
+		Description: description,
+		Completed:   false,
+		DueDate:     dueDate,
+	}
 }
